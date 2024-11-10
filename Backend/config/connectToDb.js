4e9916c0554f5168b862821a815b0b8e4046cc2b
Murchoid <kamaucoder@gmail.com>
2024-11-10 15:03:06 +0300

    Changed the URI

--- Backend/config/connectToDb.js
+++ Backend/config/connectToDb.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDb = async ()=> {
     try{
-        const connect = await mongoose.connect(process.env.MONGO_URI,{
+        const connect = await mongoose.connect(process.env.MONGO_URIONLINE,{
             useNewUrlParser:true,
             useUnifiedTopology: true
         });
